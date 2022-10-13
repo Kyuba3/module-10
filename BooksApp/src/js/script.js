f2e27ea918130454f789767ff70b0e3bd777621d
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2022-10-13 13:01:09 +0200

    actual state of project

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -7,20 +7,19 @@
     },
     containerOf: {
       book: '.books-list',
+      bookLink: '.book__image',
     },
   };
 
+  const templates = {
+    bookTemplates: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
+  };
+
   const render = function() {
-    
-    const bookTemplates = {
-      books: Handlebars.compile(
-        document.querySelector(select.templateOf.bookProduct).innerHTML
-      ),
-    };
 
     for(let book of dataSource.books){
        
-      const generatedHTML = bookTemplates.books(book);
+      const generatedHTML = templates.bookTemplates(book);
 
       const bookHtml = utils.createDOMFromHTML(generatedHTML);
        
@@ -31,4 +30,27 @@
   };
  
   render();
+  
+  const favoriteBooks = [];
+
+  const initActions = function(){
+
+    const allBooks = document.querySelectorAll(select.containerOf.bookLink);
+    console.log(allBooks);
+    for(let bookId in allBooks){
+      const book = allBooks[bookId];
+      console.log(book);
+      book.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        book.classList.add('favorite');
+        let bookAtribute = book.getElementById('data-id');
+        favoriteBooks.push(bookAtribute);
+      });
+    }
+
+    
+  };
+
+  initActions();
+
 }
